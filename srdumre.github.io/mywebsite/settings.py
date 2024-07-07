0098210aa8f61edb35be5b3302bfd46bc330a1c3
Sitaram Dumre <101350440+srdumre@users.noreply.github.com>
2024-07-07 11:59:58 +0545

    srd

--- srdumre.github.io/mywebsite/settings.py
+++ srdumre.github.io/mywebsite/settings.py
@@ -130,7 +130,7 @@ MEDIA_ROOT= BASE_DIR/'images'
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS=os.path.join(BASE_DIR,'static') ,
-STATIC_ROOT =os.path.join(BASE_DIR,'static')
+# STATIC_ROOT =os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
